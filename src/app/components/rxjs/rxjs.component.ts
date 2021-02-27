82f68853f69d97458812aadbfc6af34e45fd15e5
Mykola <mikola0672662963@gmail.com>
2021-02-27 13:40:52 +0000

    add new Stream (rxjs)

--- src/app/components/rxjs/rxjs.component.ts
+++ src/app/components/rxjs/rxjs.component.ts
@@ -1,6 +1,18 @@
 import {Component, OnInit, AfterContentInit} from '@angular/core';
-import {interval, from, of, Observable, observable, fromEvent, timer, range, Subject, BehaviorSubject, ReplaySubject} from 'rxjs';
-import {filter, take, map, scan} from 'rxjs/operators'
+import {
+  interval,
+  from,
+  of,
+  Observable,
+  observable,
+  fromEvent,
+  timer,
+  range,
+  Subject,
+  BehaviorSubject,
+  ReplaySubject
+} from 'rxjs';
+import {filter, take, map, scan, tap, takeLast, takeWhile, reduce, switchMap} from 'rxjs/operators'
 
 @Component({
   selector: 'app-rxjs',
@@ -69,11 +81,11 @@ export class RxjsComponent implements OnInit {
     // const  streamFrom$ = from( [1, 3, 4, 'bye', 'hi'])  //// послідовно віддає масив
     // streamFrom$.subscribe(value => console.log(value))
 
-  //   const streamFrom1$ = from([1, 3, 4, 'bye', 'hi', 234, 'ddd'])  /// послідовно віддає масиви (на кожній послідовності +1 значення)
-  //     .pipe(
-  //       scan((acc, value) => acc.concat(value), [])
-  //     )
-  //   streamFrom1$.subscribe(value => console.log(value))
+    // const streamFrom1$ = from([1, 3, 4, 'bye', 'hi', 234, 'ddd'])  /// послідовно віддає масиви (на кожній послідовності +1 значення)
+    //   .pipe(
+    //     scan((acc, value) => acc.concat(value), [])
+    //   )
+    // streamFrom1$.subscribe(value => console.log(value))
 
 
     // Запис
@@ -83,7 +95,7 @@ export class RxjsComponent implements OnInit {
       observable.next('First value')
 
       setTimeout(() => {
-      observable.next('value 200 ms')
+        observable.next('value 200 ms')
       }, 200)
 
 
@@ -102,56 +114,54 @@ export class RxjsComponent implements OnInit {
 
     // користування
 
-    // // streamObservable$.subscribe(
-    // //   value => console.log(value),
-    // //   error => console.log(error),
-    // //   () => console.log('complete')
-    // //   )
+    // streamObservable$.subscribe(
+    //   value => console.log(value),
+    //   error => console.log(error),
+    //   () => console.log('complete')
+    //   )
     //
     // //  OR
-    // streamObservable$.subscribe({
-    //   next(val) {
-    //     console.log(val)
-    //   },
-    //   error(err) {
-    //     console.log(err)
-    //   },
-    //   complete() {
-    //     console.log('Complete')
-    //   }
-    // })
+    // // streamObservable$.subscribe({
+    // //   next(val) {
+    // //     console.log(val)
+    // //   },
+    // //   error(err) {
+    // //     console.log(err)
+    // //   },
+    // //   complete() {
+    // //     console.log('Complete')
+    // //   }
+    // // })
 
 
-
-  // fromEvent(document.querySelector('canvas'), 'mousemove')  //////PROBLEM
-  //   .pipe(
-  //     map(e => ({
-  //       x: e.offsetX,
-  //       y: e.offsetY,
-  //       ctx: e.target.getContext('2d')
-  //     }))
-  //   )
-  //   .subscribe(position => {
-  //     console.log(position)
-  //     position.ctx.fillRect(position.x, position.y, 2, 3)
-  //   })
-  //
-  //   const clear$ = fromEvent(document.getElementById('clear'), 'click')
-  //
-  //   clear$.subscribe(() => {
-  //     const canvas = document.querySelector('canvas')
-  //     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-  //   })
-
+    // fromEvent(document.querySelector('canvas'), 'mousemove')  //////PROBLEM
+    //   .pipe(
+    //     map(e => ({
+    //       x: e.offsetX,
+    //       y: e.offsetY,
+    //       ctx: e.target.getContext('2d')
+    //     }))
+    //   )
+    //   .subscribe(position => {
+    //     console.log(position)
+    //     position.ctx.fillRect(position.x, position.y, 2, 3)
+    //   })
+    //
+    //   const clear$ = fromEvent(document.getElementById('clear'), 'click')
+    //
+    //   clear$.subscribe(() => {
+    //     const canvas = document.querySelector('canvas')
+    //     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    //   })
 
 
     //// інші приклади *interval, timer, range
 
-  //  const sus = interval(400).subscribe(value => console.log(value))
-  //
-  //   setTimeout(() => {
-  //     sus.unsubscribe()
-  //   }, 5000)
+    // const sus = interval(400).subscribe(value => console.log(value))
+    //
+    //  setTimeout(() => {
+    //    sus.unsubscribe()
+    //  }, 5000)
 
 
     // timer(2500)
@@ -162,8 +172,8 @@ export class RxjsComponent implements OnInit {
     // range(2, 10)
     //   .subscribe(value => console.log(value))
 
-
-
+    this.WorkWithOperators()
+    this.redirectionStream()
 
   }
 
@@ -172,10 +182,10 @@ export class RxjsComponent implements OnInit {
     const streamSubject$ = new Subject()
 
     streamSubject$.subscribe(
-      value => console.log('subject value: ', value ),
+      value => console.log('subject value: ', value),
       error => console.log('err', error),
       () => console.log('Complete')
-      )
+    )
 
     streamSubject$.next('H')
     streamSubject$.next('E')
@@ -187,14 +197,18 @@ export class RxjsComponent implements OnInit {
 
   //// * BehaviorSubject
   clickBehsviorSubject() {
-const streamBehaviorSubject$ = new BehaviorSubject('Sraka')
+    const streamBehaviorSubject$ = new BehaviorSubject('Sraka')
 
-       streamBehaviorSubject$.subscribe(v => console.log(v))
+    console.log(streamBehaviorSubject$.getValue(), 'GET VALUE')
+
+    streamBehaviorSubject$.subscribe(v => console.log(v, 'subscribe'))
 
     streamBehaviorSubject$.next('H')
 
-       streamBehaviorSubject$.subscribe(v => console.log(v))
+    streamBehaviorSubject$.subscribe(v => console.log(v, 'subscribe'))
     streamBehaviorSubject$.next('LL')
+
+    console.log(streamBehaviorSubject$.getValue(), 'GET VALUE')
   }
 
   /// * ReplaySubject
@@ -209,4 +223,48 @@ const streamBehaviorSubject$ = new BehaviorSubject('Sraka')
 
     streamReplaySubject$.subscribe(v => console.log(v))
   }
+
+
+  WorkWithOperators() {
+    // const intervalStream$ = interval(1000)
+    //   .pipe(                                    //// стандартний метод для трансформації стрімів
+    //     tap(v => console.log('Step', v)),  //// рахує ітерації незалежно від того чи вони виконуються з якимось результатом
+    //     // filter(v => v % 2 === 0),            //// подібно методу filter  для масивів фільтрує поточне значення стріму
+    //     // map(v => v * 10),                    //// із поточного значення стріму на даній ітерації генерує для нього нове значення
+    //     take(12),                        //// визначає кількість ітерацій в стрімі
+    //     // takeLast(6),                        /// по завершеню take витягує кількість останніх значень стріму
+    //     // scan((acc, value) => acc + value, 0) /// послідовно додає значення стріму
+    //     reduce((acc, value) => acc + value, 0) /// по завершенні стріма дає кінцеве значення
+    //     // takeWhile(v => v < 5)                //// витягує значення ітерації які підпадають під умову
+    //   )
+
+    // intervalStream$.subscribe(
+    //   value => console.log(value),
+    //   error => console.log('ERR:', error),
+    //   () => console.log('COMPLETE')
+    // )
+  }
+
+
+  //// перенаправлення стрімів (або використання стрімів всередені стрімів)
+
+  redirectionStream() {
+      // fromEvent(document, 'click')
+      //   .pipe(
+      //    switchMap(event => {
+      //      return interval(1000)
+      //        .pipe(
+      //          tap(v => console.log(' Itar number:', v)),
+      //          take(5),
+      //          reduce((acc, value) => acc + value, 0)
+      //        )
+      //    })
+      //   )
+      //   .subscribe(
+      //     next => console.log(next, 'counter complete'),
+      //     error => console.log(error),
+      //     () => console.log('cComplete ura!')
+      //   )
+  }
+
 }
